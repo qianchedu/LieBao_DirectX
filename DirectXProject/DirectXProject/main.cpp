a683571697c114d7b0734363f5d1e8a7b8d3fc44
qianchedu <13713659927@163.com>
2019-10-26 12:59:55 +0800

    04   第1章 创建和显示Direct3D窗口 (第13页) 创建D3D窗口

--- DirectXProject/DirectXProject/main.cpp
+++ DirectXProject/DirectXProject/main.cpp
@@ -1,7 +1,19 @@
 #include <Windows.h>
+#include <d3d9.h>
+#pragma comment(lib,"d3d9.lib");
+#pragma comment(lib,"d3dx9.lib");
+
 
 #define WINDOW_CLASS "UGPDX"
 #define WINDOW_TITLE "姆姆游戏引擎demo"
+
+
+LPDIRECT3D9 g_D3D = NULL;
+LPDIRECT3DDEVICE9 g_D3DDevice = NULL;
+
+bool InitializeD3D(HWND hWnd);
+void Shutdown();
+void RenderScene();
 
 LRESULT WINAPI MsgProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam)
 {
@@ -50,7 +62,7 @@ int WINAPI WinMain(HINSTANCE hInst,
 		hInst,
 		NULL,				//图标
 		NULL,				//鼠标
-		(HBRUSH)GetStockObject(WHITE_BRUSH),//NULL,				//背景色
+		NULL,				//背景色//(HBRUSH)GetStockObject(WHITE_BRUSH),
 		NULL,				//菜单
 		WINDOW_CLASS,			//窗口类名称
 		NULL				//创建窗口的
@@ -72,23 +84,76 @@ int WINAPI WinMain(HINSTANCE hInst,
 			hInst,
 			NULL);
 
-	ShowWindow(hWnd,SW_SHOWDEFAULT);
-	UpdateWindow(hWnd);
-
-	MSG msg;
-	//清空
-	ZeroMemory(&msg,sizeof(msg));
-
-	//消息循环
-	while (msg.message != WM_QUIT)
+	if (InitializeD3D(hWnd))
 	{
-		if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))
+
+		ShowWindow(hWnd,SW_SHOWDEFAULT);
+		UpdateWindow(hWnd);
+
+		MSG msg;
+		//清空
+		ZeroMemory(&msg,sizeof(msg));
+
+		//消息循环
+		while (msg.message != WM_QUIT)
 		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
+			if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			else 
+			{
+				//没有消息要处理，就可以做其他工作；
+				RenderScene();
+			}
 		}
 	}
-
+	
+	Shutdown();
 	//取消注册-释放资源
 	UnregisterClass(WINDOW_CLASS,hInst);
+}
+
+
+
+bool InitializeD3D(HWND hWnd)
+{
+	D3DDISPLAYMODE displayMode;
+	g_D3D = Direct3DCreate9(D3D_SDK_VERSION);
+	if (g_D3D == NULL)return false;
+	if (FAILED(g_D3D->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &displayMode)))
+	{
+		return false;
+	}
+
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp,sizeof(d3dpp));
+	d3dpp.Windowed = TRUE;
+	d3dpp.BackBufferFormat = displayMode.Format;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	if (FAILED(g_D3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &g_D3DDevice)))
+	{
+		return false;
+	}
+	return true;
+
+}
+
+
+void Shutdown()
+{
+	if (g_D3DDevice != NULL)g_D3DDevice->Release();
+	if (g_D3D != NULL)g_D3D->Release();
+}
+
+
+void RenderScene()
+{
+	g_D3DDevice->Clear(0,NULL,D3DCLEAR_TARGET,D3DCOLOR_XRGB(0,0,0),1.0F,0);
+	g_D3DDevice->BeginScene();
+	//输出3D图
+	g_D3DDevice->EndScene();
+
+	g_D3DDevice->Present(NULL,NULL,NULL,NULL);
 }
